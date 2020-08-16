2168e6385582cda9def30a13c63474022be329d9
JFL110 <typus24@hotmail.com>
2020-08-16 09:49:29 +0100

    Initial commit of repository setup

---
+++ src/test/java/org/jfl110/TestNothing.java
@@ -0,0 +1,19 @@
+package org.jfl110;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Test that should always pass to verify repository setup
+ * 
+ * @author jim
+ *
+ */
+public class TestNothing {
+
+	@Test
+	public void testNothing() {
+		assertEquals("a", "a");
+	}
+}
